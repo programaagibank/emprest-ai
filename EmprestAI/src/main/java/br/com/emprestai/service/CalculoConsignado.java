96e2fdf54091ba775aa3163ab5bcf41c8093eb71
CarollinaGuedes <carollinaguedes@hotmail.com>
2025-03-10 11:23:27 -0300

    feat(service): :sparkles: Adicionado metodo na Classe CalculoConsignado

--- EmprestAI/src/main/java/br/com/emprestai/service/CalculoConsignado.java
+++ EmprestAI/src/main/java/br/com/emprestai/service/CalculoConsignado.java
@@ -4,7 +4,7 @@ public class CalculoConsignado {
 
     // 11.1.1 Margem Consignável
     public static void margemConsignavel(double renda, double valorEmprestimo, int parcelas) {
-        double margem = renda * 0.35; 
+        double margem = renda * 0.35;
         double parcela = valorEmprestimo / parcelas;
         if (parcela <= margem) {
             System.out.println("Parcela ok! Dentro da margem de 35% da renda.");
@@ -32,3 +32,39 @@ public class CalculoConsignado {
         }
     }
 }
+
+//11.1.4 Taxas de Juros
+public static void taxaJuros(double quantidadeDeParcelas, double taxaMensal) {
+    if (quantidadeDeParcelas >= 24 && quantidadeDeParcelas <= 92) {
+        System.out.println("Quantidade minima e máxima de parcelas para parcelas consignado.");
+    }
+
+    if (taxaMensal >= 1.80 && taxaMensal <= 2.14) {
+        System.out.println("Taxa mínima e taxa máxima para empréstimo consignado");
+    }
+}
+
+//11.1.5 Tipo de vinculo
+public static void tipoVinculo(String categoria) {
+    if (categoria.equals("Aposentado")) {
+        System.out.println("Categoria: Aposentado");
+    } else if (categoria.equals("Servidor Público")) {
+        System.out.println("Categoria: Servidor Público");
+    } else if (categoria.equals("Pensionista")) {
+        System.out.println("Categoria: Pensionista");
+    } else {
+        System.out.println("Categoria desconhecida");
+    }
+}
+
+
+//11.1.6 Carência
+public static void carencia(double diasCarencia) {
+    if (diasCarencia <= 60) {
+        System.out.println("Carência ok. Máximo 60 dias.");
+    } else {
+        System.out.println("Carência maior que 60 dias!");
+    }
+}
+
+

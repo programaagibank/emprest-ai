ff4e34910e3fb6922788e8e117b78cfecbfbeb31
josdcosta <josue.davi.costa@outlook.com>
2025-03-10 09:10:21 -0300

    feat(ConfigVariaveisAmbiente): :sparkles: Adicionado classe ConfigVariaveisAmbiente

---
+++ EmprestAI/src/main/java/br/com/emprestai/util/ConfigVariaveisAmbiente.java
@@ -0,0 +1,194 @@
+package br.com.emprestai.util;
+
+public class ConfigVariaveisAmbiente {
+
+    private double jurosMinimoPessoal;
+    private double jurosMaximoPessoal;
+    private double jurosMinimoConsignado;
+    private double jurosMaximoConsignado;
+    private double valorMinimoPessoal;
+    private double valorMaximoPessoal;
+    private double valorMinimoConsignado;
+    private double prazoMinimoPessoal;
+    private double prazoMaximoPessoal;
+    private double prazoMinimoConsignado;
+    private double prazoMaximoConsignado;
+    private double carenciaMaximaPessoal;
+    private double carenciaMaximaConsignado;
+    private double idadeMaximaConsignado;
+    private double idadeMaximaPessoal;
+    private double margemConsignavel;
+    private double iof;
+    private double percentualRendaPessoal;
+    private double percentualMinimoRefinanciamento;
+    private double percentualJurosMora;
+    private double percentualMultaAtraso;
+
+    public double getJurosMaximoPessoal() {
+        return jurosMaximoPessoal;
+    }
+
+    public void setJurosMaximoPessoal(double jurosMaximoPessoal) {
+        this.jurosMaximoPessoal = jurosMaximoPessoal;
+    }
+
+    public double getJurosMinimoPessoal() {
+        return jurosMinimoPessoal;
+    }
+
+    public void setJurosMinimoPessoal(double jurosMinimoPessoal) {
+        this.jurosMinimoPessoal = jurosMinimoPessoal;
+    }
+
+    public double getJurosMinimoConsignado() {
+        return jurosMinimoConsignado;
+    }
+
+    public void setJurosMinimoConsignado(double jurosMinimoConsignado) {
+        this.jurosMinimoConsignado = jurosMinimoConsignado;
+    }
+
+    public double getJurosMaximoConsignado() {
+        return jurosMaximoConsignado;
+    }
+
+    public void setJurosMaximoConsignado(double jurosMaximoConsignado) {
+        this.jurosMaximoConsignado = jurosMaximoConsignado;
+    }
+
+    public double getValorMinimoPessoal() {
+        return valorMinimoPessoal;
+    }
+
+    public void setValorMinimoPessoal(double valorMinimoPessoal) {
+        this.valorMinimoPessoal = valorMinimoPessoal;
+    }
+
+    public double getValorMaximoPessoal() {
+        return valorMaximoPessoal;
+    }
+
+    public void setValorMaximoPessoal(double valorMaximoPessoal) {
+        this.valorMaximoPessoal = valorMaximoPessoal;
+    }
+
+    public double getValorMinimoConsignado() {
+        return valorMinimoConsignado;
+    }
+
+    public void setValorMinimoConsignado(double valorMinimoConsignado) {
+        this.valorMinimoConsignado = valorMinimoConsignado;
+    }
+
+    public double getPrazoMinimoPessoal() {
+        return prazoMinimoPessoal;
+    }
+
+    public void setPrazoMinimoPessoal(double prazoMinimoPessoal) {
+        this.prazoMinimoPessoal = prazoMinimoPessoal;
+    }
+
+    public double getPrazoMaximoPessoal() {
+        return prazoMaximoPessoal;
+    }
+
+    public void setPrazoMaximoPessoal(double prazoMaximoPessoal) {
+        this.prazoMaximoPessoal = prazoMaximoPessoal;
+    }
+
+    public double getPrazoMinimoConsignado() {
+        return prazoMinimoConsignado;
+    }
+
+    public void setPrazoMinimoConsignado(double prazoMinimoConsignado) {
+        this.prazoMinimoConsignado = prazoMinimoConsignado;
+    }
+
+    public double getPrazoMaximoConsignado() {
+        return prazoMaximoConsignado;
+    }
+
+    public void setPrazoMaximoConsignado(double prazoMaximoConsignado) {
+        this.prazoMaximoConsignado = prazoMaximoConsignado;
+    }
+
+    public double getCarenciaMaximaPessoal() {
+        return carenciaMaximaPessoal;
+    }
+
+    public void setCarenciaMaximaPessoal(double carenciaMaximaPessoal) {
+        this.carenciaMaximaPessoal = carenciaMaximaPessoal;
+    }
+
+    public double getCarenciaMaximaConsignado() {
+        return carenciaMaximaConsignado;
+    }
+
+    public void setCarenciaMaximaConsignado(double carenciaMaximaConsignado) {
+        this.carenciaMaximaConsignado = carenciaMaximaConsignado;
+    }
+
+    public double getIdadeMaximaConsignado() {
+        return idadeMaximaConsignado;
+    }
+
+    public void setIdadeMaximaConsignado(double idadeMaximaConsignado) {
+        this.idadeMaximaConsignado = idadeMaximaConsignado;
+    }
+
+    public double getIdadeMaximaPessoal() {
+        return idadeMaximaPessoal;
+    }
+
+    public void setIdadeMaximaPessoal(double idadeMaximaPessoal) {
+        this.idadeMaximaPessoal = idadeMaximaPessoal;
+    }
+
+    public double getMargemConsignavel() {
+        return margemConsignavel;
+    }
+
+    public void setMargemConsignavel(double margemConsignavel) {
+        this.margemConsignavel = margemConsignavel;
+    }
+
+    public double getIof() {
+        return iof;
+    }
+
+    public void setIof(double iof) {
+        this.iof = iof;
+    }
+
+    public double getPercentualRendaPessoal() {
+        return percentualRendaPessoal;
+    }
+
+    public void setPercentualRendaPessoal(double percentualRendaPessoal) {
+        this.percentualRendaPessoal = percentualRendaPessoal;
+    }
+
+    public double getPercentualMinimoRefinanciamento() {
+        return percentualMinimoRefinanciamento;
+    }
+
+    public void setPercentualMinimoRefinanciamento(double percentualMinimoRefinanciamento) {
+        this.percentualMinimoRefinanciamento = percentualMinimoRefinanciamento;
+    }
+
+    public double getPercentualJurosMora() {
+        return percentualJurosMora;
+    }
+
+    public void setPercentualJurosMora(double percentualJurosMora) {
+        this.percentualJurosMora = percentualJurosMora;
+    }
+
+    public double getPercentualMultaAtraso() {
+        return percentualMultaAtraso;
+    }
+
+    public void setPercentualMultaAtraso(double percentualMultaAtraso) {
+        this.percentualMultaAtraso = percentualMultaAtraso;
+    }
+}

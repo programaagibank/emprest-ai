7266c9887c12a44860f5cc49a6d7a9afb5ba0253
Victor-Ferreira <victoraugusto103587@gmail.com>
2025-03-13 01:12:50 -0300

    feat(model): :sparkles: adcionado getters e setters para a entidade emprestimo

--- EmprestAI/src/main/java/br/com/emprestai/model/Emprestimo.java
+++ EmprestAI/src/main/java/br/com/emprestai/model/Emprestimo.java
@@ -25,9 +25,11 @@ public class Emprestimo {
     private double taxaMulta;
     private long idEmprestimoOrigem;
 
+    //Construtor vazio
     public Emprestimo() {
     }
 
+    //Construtor com parâmetros
     public Emprestimo(long idCliente, long idContrato, BigDecimal valorTotal, int quantidadeParcelas, double juros, LocalDate dataInicio,
                       StatusEmpEnum idStatusEmprestimo, BigDecimal valorSeguro, BigDecimal valorIOF, BigDecimal outrosCustos,
                       LocalDate dataContratacao, MotivosEncerramentosEmpEnum idMotivoEncerramento, double jurosMora, double taxaMulta, long idEmprestimoOrigem){
@@ -48,5 +50,117 @@ public class Emprestimo {
         this.idEmprestimoOrigem = idEmprestimoOrigem;
     }
 
+    // Getters e setters
+    public long getIdCliente(){
+        return idCliente;
+    }
+    public void setIdCliente(long idCliente){
+        this.idCliente = idCliente;
+    }
+
+    public long getIdContrato(){
+        return idContrato;
+    }
+    public void setIdContrato(long idContrato){
+        this.idContrato = idContrato;
+    }
+
+    public BigDecimal getValorTotal(){
+        return valorTotal;
+    }
+    public void setValorTotal(BigDecimal valorTotal){
+        this.valorTotal = valorTotal;
+    }
+
+    public int getQuantidadeParcelas(){
+        return quantidadeParcelas;
+    }
+    public void setQuantidadeParcelas(int quantidadeParcelas){
+        this.quantidadeParcelas = quantidadeParcelas;
+    }
+
+    public double getJuros(){
+        return juros;
+    }
+    public void setJuros(double juros){
+        this.juros = juros;
+    }
+
+    public LocalDate getDataInicio(){
+        return dataInicio;
+    }
+    public void setDataInicio(LocalDate dataInicio){
+        this.dataInicio = dataInicio;
+    }
+
+    public StatusEmpEnum getIdStatusEmprestimo(){
+        return idStatusEmprestimo;
+    }
+    public void setIdStatusEmprestimo(StatusEmpEnum idStatusEmprestimo){
+        this.idStatusEmprestimo = idStatusEmprestimo;
+    }
+
+    public TipoEmpEnum getIdTipoEmprestimo(){
+        return idTipoEmprestimo;
+    }
+    public void setIdTipoEmprestimo(TipoEmpEnum idTipoEmprestimo){
+        this.idTipoEmprestimo = idTipoEmprestimo;
+    }
+
+    public BigDecimal getValorSeguro(){
+        return valorSeguro;
+    }
+    public void setValorSeguro(BigDecimal valorSeguro){
+        this.valorSeguro = valorSeguro;
+    }
+
+    public BigDecimal getValorIOF(){
+        return valorIOF;
+    }
+    public void setValorIOF(BigDecimal valorIOF){
+        this.valorIOF = valorIOF;
+    }
+
+    public BigDecimal getOutrosCustos(){
+        return outrosCustos;
+    }
+    public void setOutrosCustos(BigDecimal outrosCustos){
+        this.outrosCustos = outrosCustos;
+    }
+
+    public LocalDate getDataContratacao(){
+        return dataContratacao;
+    }
+    public void setDataContratacao(LocalDate dataContratacao){
+        this.dataContratacao = dataContratacao;
+    }
+
+    public MotivosEncerramentosEmpEnum getIdMotivoEncerramento(){
+        return idMotivoEncerramento;
+    }
+    public void setIdMotivoEncerramento(MotivosEncerramentosEmpEnum idMotivoEncerramento){
+        this.idMotivoEncerramento = idMotivoEncerramento;
+    }
+
+    public double getJurosMora(){
+        return jurosMora;
+    }
+    public void setJurosMora(double jurosMora){
+        this.jurosMora = jurosMora;
+    }
+
+    public double getTaxaMulta(){
+        return taxaMulta;
+    }
+    public void setTaxaMulta(double taxaMulta){
+        this.taxaMulta = taxaMulta;
+    }
+
+    public long getIdEmprestimoOrigem(){
+        return idEmprestimoOrigem;
+    }
+    public void setIdEmprestimoOrigem(long idEmprestimoOrigem){
+        this.idEmprestimoOrigem = idEmprestimoOrigem;
+    }
 }
 

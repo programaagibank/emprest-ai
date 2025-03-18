2938fb357bcb6db77e3debc333abc9fbdd15a88a
josdcosta <josue.davi.costa@outlook.com>
2025-03-17 21:25:05 -0300

    feat(controller): :sparkles: Adicionado classe LoginController

--- EmprestAI/src/main/java/br/com/emprestai/service/LoginService.java
+++ EmprestAI/src/main/java/br/com/emprestai/service/LoginService.java
@@ -1,5 +1,17 @@
 package br.com.emprestai.service;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class LoginService {
+
+    public static String senhaHash(String plainPassword) {
+        // Gera um hash com um salt automático (work factor padrão é 10, mas pode ser ajustado)
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+    }
+
+    public static boolean checarSenha(String senhaEnviada, String senhaHash) {
+        // Verifica se a senha em texto puro corresponde ao hash
+        return BCrypt.checkpw(senhaEnviada, senhaHash);
+    }
 
 }

fe1ed77faf790f5f228cdba624044fe1eb4ce2bc
dalleth-martins <essi_dalleth@outlook.com>
2025-03-18 09:26:57 -0300

    fix(model): :bug: Corrigido no Login

--- EmprestAI/src/main/java/br/com/emprestai/model/Login.java
+++ EmprestAI/src/main/java/br/com/emprestai/model/Login.java
@@ -3,14 +3,10 @@ package br.com.emprestai.model;
 public class Login {
     private String usuarioBanco;
     private String senhaBanco;
-    private String usuarioRecebido;
-    private String senhaRecebido;
 
-    public Login(String usuarioBanco, String senhaBanco, String usuarioRecebido, String senhaRecebido) {
+    public Login(String usuarioBanco, String senhaBanco) {
         this.usuarioBanco = usuarioBanco;
         this.senhaBanco = senhaBanco;
-        this.usuarioRecebido = usuarioRecebido;
-        this.senhaRecebido = senhaRecebido;
     }
 
     public String getUsuarioBanco() {
@@ -29,29 +25,11 @@ public class Login {
         this.senhaBanco = senhaBanco;
     }
 
-    public String getUsuarioRecebido() {
-        return usuarioRecebido;
-    }
-
-    public void setUsuarioRecebido(String usuarioRecebido) {
-        this.usuarioRecebido = usuarioRecebido;
-    }
-
-    public String getSenhaRecebido() {
-        return senhaRecebido;
-    }
-
-    public void setSenhaRecebido(String senhaRecebido) {
-        this.senhaRecebido = senhaRecebido;
-    }
-
-    @Override
+   @Override
     public String toString() {
         return "Login{" +
                 "usuarioBanco='" + usuarioBanco + '\'' +
                 ", senhaBanco='" + senhaBanco + '\'' +
-                ", usuarioRecebido='" + usuarioRecebido + '\'' +
-                ", senhaRecebido='" + senhaRecebido + '\'' +
                 '}';
     }
 }

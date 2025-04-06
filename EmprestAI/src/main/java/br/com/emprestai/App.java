552224fb852128d3043d19dcaede8d6cc507bdd2
dalleth-martinss <essi_dalleth@outlook.com>
2025-04-06 13:32:04 -0300

    [view] :bug: Alteracao na tela login

--- EmprestAI/src/main/java/br/com/emprestai/App.java
+++ EmprestAI/src/main/java/br/com/emprestai/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 640); // Mobile phone resolution
+        Scene scene = new Scene(fxmlLoader.load(), 400, 680); // Mobile phone resolution
         stage.setTitle("Emprest.AI - Gerenciador de Empréstimos");
         stage.setScene(scene);
         stage.show();
